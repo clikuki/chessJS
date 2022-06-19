65874421d7948c76a5510aa0f47591ea7d8f5a47
Clikuki <73450123+clikuki@users.noreply.github.com>
2022-06-19 10:25:08 +0800

    init, dom structure created

---
+++ src/loadImages.ts
@@ -0,0 +1,38 @@
+interface ImagesContainer {
+	kl: string;
+	kd: string;
+	ql: string;
+	qd: string;
+	rl: string;
+	rd: string;
+	bl: string;
+	bd: string;
+	nl: string;
+	nd: string;
+	pl: string;
+	pd: string;
+}
+let images = {} as ImagesContainer;
+let hasBeenLoaded = false;
+export function loadImages() {
+	if (hasBeenLoaded) return images;
+	const imgContainer = document.createElement('div');
+	imgContainer.classList.add('imagePreloading');
+	const extension = 'png';
+	const basePath = 'assets/';
+	const pieceChars = 'kqrbnp'.split('');
+	const clrChars = ['d', 'l'];
+	for (const pieceChar of pieceChars) {
+		for (const clrChar of clrChars) {
+			const name = `${pieceChar}${clrChar}` as keyof ImagesContainer;
+			const imgSrc = `${basePath}${name}.${extension}`;
+			const image = document.createElement('img') as HTMLImageElement;
+			image.src = imgSrc;
+			imgContainer.appendChild(image);
+			images[name] = imgSrc;
+		}
+	}
+	document.body.appendChild(imgContainer);
+	hasBeenLoaded = true;
+	return images;
+}
