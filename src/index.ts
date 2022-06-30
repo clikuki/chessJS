533227bb7aeabab52c51cc0c49daab79ce7d5451
Clikuki <73450123+clikuki@users.noreply.github.com>
2022-06-30 22:48:45 +0800

    FEATURE: gui now highlights from and to squares CHANGE: rewrote whole gui system, img pieces are no longer children of tiles

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,34 @@ import { readFen } from './fen.js';
 import { ImageSrcContainer, imgSrcs } from './loadImages.js';
 import { getPieceCharClr } from './utility.js';
 
-// Get tile size
-const computedStyles = getComputedStyle(document.documentElement);
-const tileSizeInRem = parseFloat(
-	computedStyles.getPropertyValue('--tile-size'),
-);
-const fontSize = parseFloat(computedStyles.fontSize);
-const tileSize = tileSizeInRem * fontSize;
-
-// Load up board
-// const startFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-// const startFen = '8/4k3/8/8/4R3/8/8/4K3 b - - 0 1';
-const startFen = 'r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1';
-const board = readFen(startFen);
-board.generateMoves();
-
-// Create dom representation
-const grid = document.createElement('div');
-const imgs: (HTMLImageElement | null)[] = [];
-const tiles: HTMLElement[] = [];
-grid.classList.add('grid');
-for (let j = 0; j < 8; j++) {
-	for (let i = 0; i < 8; i++) {
-		const tile = document.createElement('div');
-		tile.classList.add('tile', (i + j) % 2 ? 'light' : 'dark');
-		const index = i + j * 8;
-
-		if (board.pieces[index]) {
-			const char = board.pieces[index]!;
-			const name = `${char.toLowerCase()}${
-				char.toLowerCase() === char ? 'd' : 'l'
-			}`;
-			const imgSrc = imgSrcs[name as keyof ImageSrcContainer];
-			const img = document.createElement('img');
-			img.src = imgSrc;
-			tile.appendChild(img);
-			imgs[index] = img;
-		} else imgs[index] = null;
-
-		tiles[index] = tile;
-		grid.appendChild(tile);
-	}
-}
-document.body.appendChild(grid);
-
-function getXY(e: MouseEvent) {
+function getMouseEventXY(e: MouseEvent) {
 	return {
 		x: e.pageX,
 		y: e.pageY,
 	};
 }
 
+function sqToPos(sq: number) {
+	return [(sq % 8) * tileSize, Math.floor(sq / 8) * tileSize] as const;
+}
+
 function setXYOnElement(element: HTMLElement, x: number, y: number) {
 	element.style.setProperty('--x', `${x}px`);
 	element.style.setProperty('--y', `${y}px`);
+}
+
+function setTileXYOnElement(element: HTMLElement, x: number, y: number) {
+	element.setAttribute('data-tile-x', `${x}`);
+	element.setAttribute('data-tile-y', `${y}`);
+}
+
+function getElementTileXY(element: HTMLElement) {
+	const x = element.getAttribute('data-tile-x');
+	const y = element.getAttribute('data-tile-y');
+	return {
+		x: x === null ? null : +x,
+		y: y === null ? null : +y,
+	};
 }
 
 function setDragging(element: HTMLElement, draggable: boolean) {
@@ -82,10 +56,63 @@ function canTakeSq(startSq: number, targetSq: number) {
 	return !captured || getPieceCharClr(captured) !== getPieceCharClr(moved);
 }
 
+function updateTileHighlights(from: number, to: number) {
+	const className = 'highlight';
+	tiles.forEach((tile, i) => {
+		if ([from, to].includes(i)) tile.classList.add(className);
+		else tile.classList.remove(className);
+	});
+}
+
+// Get tile size
+const computedStyles = getComputedStyle(document.documentElement);
+const tileSizeInRem = parseFloat(
+	computedStyles.getPropertyValue('--tile-size'),
+);
+const fontSize = parseFloat(computedStyles.fontSize);
+const tileSize = tileSizeInRem * fontSize;
+
+// Load up board
+const startFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const board = readFen(startFen);
+board.generateMoves();
+
+// Create dom representation
+const grid = document.createElement('div');
+const imgs: (HTMLImageElement | null)[] = [];
+const tiles: HTMLElement[] = [];
+grid.classList.add('board');
+for (let j = 0; j < 8; j++) {
+	for (let i = 0; i < 8; i++) {
+		const tile = document.createElement('div');
+		tile.classList.add('tile', (i + j) % 2 ? 'light' : 'dark');
+		const index = i + j * 8;
+
+		if (board.pieces[index]) {
+			const char = board.pieces[index]!;
+			const name = `${char.toLowerCase()}${
+				char.toLowerCase() === char ? 'd' : 'l'
+			}`;
+			const imgSrc = imgSrcs[name as keyof ImageSrcContainer];
+			const img = document.createElement('img');
+			img.src = imgSrc;
+			img.classList.add('piece');
+			imgs[index] = img;
+			setXYOnElement(img, i * tileSize, j * tileSize);
+			setTileXYOnElement(img, i * tileSize, j * tileSize);
+			grid.appendChild(img);
+		} else imgs[index] = null;
+
+		tiles[index] = tile;
+		grid.appendChild(tile);
+	}
+}
+document.body.appendChild(grid);
+
 let startSq: number | null = null;
 grid.addEventListener('mousedown', (e) => {
 	e.stopPropagation();
-	const { x, y } = getXY(e);
+	const { x, y } = getMouseEventXY(e);
 	const sq = getSq(x, y);
 	const piece = board.pieces[sq];
 	if (!piece || getPieceCharClr(piece) !== board.activeClr) return;
@@ -96,39 +123,43 @@ grid.addEventListener('mousedown', (e) => {
 });
 grid.addEventListener('mousemove', (e) => {
 	if (startSq === null) return;
-	const { x, y } = getXY(e);
+	const { x, y } = getMouseEventXY(e);
 	const img = imgs[startSq]!;
 	setXYOnElement(img, x, y);
 });
 grid.addEventListener('mouseup', (e) => {
 	if (startSq === null) return;
 	const img = imgs[startSq]!;
-	const { x, y } = getXY(e);
+	const { x, y } = getMouseEventXY(e);
 	const sq = getSq(x, y);
 	const move = findMove(startSq, sq);
 	if (move && canTakeSq(startSq, sq)) {
 		imgs[startSq] = null;
 		if (board.enpassantSq === sq) {
 			const captureSq = sq + (board.activeClr ? 8 : -8);
-			imgs[captureSq]!.remove();
+			imgs[captureSq]?.remove();
 			imgs[captureSq] = null;
 		}
 		imgs[sq]?.remove();
 		imgs[sq] = img;
-		tiles[sq].appendChild(img);
+		setTileXYOnElement(img, ...sqToPos(sq));
 		if (move.options.isCastling) {
 			const side = move.options.castlingSide!;
 			const oldRookPosition = move.targetSq + (side ? 1 : -2);
 			const newRookPosition = move.targetSq + (side ? -1 : 1);
 			const rookImg = imgs[oldRookPosition]!;
-			rookImg.remove();
-			tiles[newRookPosition].appendChild(rookImg);
 			imgs[oldRookPosition] = null;
 			imgs[newRookPosition] = rookImg;
+			const [x, y] = sqToPos(newRookPosition);
+			setXYOnElement(rookImg, x, y);
+			setTileXYOnElement(rookImg, x, y);
 		}
 		board.makeMove(move);
+		updateTileHighlights(move.startSq, move.targetSq);
 	}
 
+	const { x: tileX, y: tileY } = getElementTileXY(img);
+	setXYOnElement(img, tileX!, tileY!);
 	setDragging(img, false);
 	startSq = null;
 });
